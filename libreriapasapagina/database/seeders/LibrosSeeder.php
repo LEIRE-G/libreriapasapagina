0caa53a62503a60934860f44ac34c3e5e7347663
LEIRE-G <leiregarcianaves@alumnos.tuniversformacion.es>
2026-01-22 09:48:09 +0100

    crear las migraciones para mysql

--- libreriapasapagina/database/seeders/LibrosSeeder.php
+++ libreriapasapagina/database/seeders/LibrosSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Libro;
 
 class LibrosSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class LibrosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // dos libros 
+        Libro::create([
+            'titulo' => 'El principito',
+            'autor' => 'Antoine de Saint-Exupéry',
+            'paginas' => 247,
+            'precio' => 15.99
+        ]);
+
+        Libro::create([
+            'titulo' => 'Don Quijote de la Mancha',
+            'autor' => 'Miguel de Cervantes',
+            'paginas' => 843,
+            'precio' => 22.99
+        ]);
     }
 }
